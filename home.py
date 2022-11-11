d3f592fde29a4eaa701551db39ddfe015c8a6f73
kalyan221 <117138506+kalyan221@users.noreply.github.com>
2022-11-11 12:53:58 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="Python Classes", page_icon=":house", layout="centered", initial_sidebar_state="auto")
+st.title("Welcome To Python Classes Website")
+st.image("IMG-20220421-WA0011-removebg-preview_1.jpeg")
+st.subheader("Welcome Friends")
+st.text(""" This is the python classes website.
+                    First of all again welcome.
+                    This is the beginning of our journey.""")
